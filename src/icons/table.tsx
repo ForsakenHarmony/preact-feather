339afad40197951ed70dd210bff543777a29b9a8
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2026-06-03 00:32:17 +0200

    chore: maintenance (#24)

---
+++ src/icons/table.tsx
@@ -0,0 +1,28 @@
+import { h, FunctionalComponent } from 'preact';
+import { FeatherProps } from '../types';
+
+export const Table: FunctionalComponent<FeatherProps> = (
+	props: FeatherProps
+) => {
+	const color = props.color || 'currentColor';
+	const size = props.size || 24;
+	delete props.color;
+	delete props.size;
+
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			{...(props as Omit<typeof props, 'color' | 'size'>)}
+		>
+			<path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18"></path>
+		</svg>
+	);
+};
